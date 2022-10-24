1d708efaa5fd3d875ad6bc864ccf40092002e3a2
Dmitriy Nekludov <nekludov.d.a@gmail.com>
2022-10-24 20:44:46 +0300

    Add articles and filters scripts

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,9 @@
 import 'focus-visible';
 import onDocumentReady from './helpers/onDocumentReady';
 import lazyImages from './modules/lazyimages';
+import createArticles from './modules/articles';
 
 onDocumentReady(() => {
   lazyImages();
+  createArticles();
 });
