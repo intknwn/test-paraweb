ca918ebe74af91c528c7383ffabf770ebd887c3f
Dmitriy Nekludov <nekludov.d.a@gmail.com>
2022-10-24 23:33:20 +0300

    Fix a bug

--- src/scripts/modules/sticky-filters.js
+++ src/scripts/modules/sticky-filters.js
@@ -1,15 +1,16 @@
+const ARTICLES_CONTAINER_CLASS = `.articles`;
 const FILTERS_CONTAINER_CLASS = `.filters`;
 const PAGE_HEADER_CLASS = `.page-header`;
 const STICKY_FILTERS_CLASS = `filters--sticky`;
 
-const headerOffset = document.querySelector(PAGE_HEADER_CLASS).offsetHeight;
+const header = document.querySelector(PAGE_HEADER_CLASS);
+const articles = document.querySelector(ARTICLES_CONTAINER_CLASS);
 const filters = document.querySelector(FILTERS_CONTAINER_CLASS);
-const filtersOffsetTop = filters.offsetTop;
 
 const handleScroll = () => {
-  if (window.pageYOffset + headerOffset > filtersOffsetTop) {
+  if (window.pageYOffset + header.offsetHeight > articles.offsetTop) {
     filters.classList.add(STICKY_FILTERS_CLASS);
-    filters.style.top = `${headerOffset}px`;
+    filters.style.top = `${header.offsetHeight}px`;
   } else {
     filters.classList.remove(STICKY_FILTERS_CLASS);
     filters.style.top = ``;
